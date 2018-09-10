56e9972596960df2f29f446057f98b53328885ea
Oliver Baum <oliver@baums.org.uk>
2018-09-10 16:43:19 +0100

    Completed steps 1-6

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,13 @@
 class Oystercard
   attr_reader :balance
+  BALANCE_CAP = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    raise "Top-up can't exceed card limit of £#{BALANCE_CAP}" if @balance + amount > BALANCE_CAP
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,9 +5,15 @@ describe Oystercard do
     end
 
     context '#top_up' do
+
       it 'increases the balance by top up amount' do
         subject.top_up(5)
         expect(subject::balance).to be 5
+      end
+
+      it 'raises error when top_up would make balance > 90' do
+        expect{subject.top_up(Oystercard::BALANCE_CAP + 1)}.to raise_error(
+          "Top-up can't exceed card limit of £#{Oystercard::BALANCE_CAP}")
       end
     end
 end
